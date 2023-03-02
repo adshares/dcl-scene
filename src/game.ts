2f7310486741676d41ae94f591e4b86eada1a59e
Maciej Pilarczyk <m.pilarczyk@outlook.com>
2023-03-02 17:29:30 +0100

    Stand

--- src/game.ts
+++ src/game.ts
@@ -1,56 +1,30 @@
-// import AdsharesBanner from '@adshares/decentraland'
-// import AdsharesBanner from '../../decentraland/src/banner'
+import {PlainPlacement, SupplyAgent} from '../../decentraland/src/index'
 
-import { Chain, SupplyAgent, PlainPlacement } from '../../decentraland/src/index'
+const agent = SupplyAgent.fromWallet('https://app.web3ads.net', 'ads', '0001-00000000-9B6F')
+// const agent = new SupplyAgent('https://app.web3ads.net', 'e39f6593-578e-41f0-8d06-88aff41c6a19')
 
-const agent = SupplyAgent.fromWallet('https://app.web3ads.net', Chain.ADS, '0001-000000F1-6451')
-// const agent = new SupplyAgent('https://app.web3ads.net', 'd64bf2a15c5de2e33b20c4b6100c2d5d')
-
-const placement1 = new PlainPlacement('unit1')
-placement1.addComponent(new Transform({
-  position: new Vector3(4, 0, 8),
-  scale: new Vector3(2, 2, 0.1),
+const stand = new Entity()
+stand.addComponent(new GLTFShape('models/stand.glb'))
+stand.addComponent(new Transform({
+    position: new Vector3(8, 0, 8),
 }))
-engine.addEntity(placement1)
 
-// const placement2 = new PlainPlacement('unit2', ['image', 'video'], ['image/jpeg', 'image/png', 'video/mp4'])
-// placement2.addComponent(new Transform({
-//   position: new Vector3(8, 0, 4),
-//   scale: new Vector3(2, 2, 0.1),
-// }))
-// engine.addEntity(placement2)
+const frontPlacement = new PlainPlacement('Stand front', {
+    position: new Vector3(0, 2.4, -0.28),
+    width: 1.88,
+    ratio: '9:16',
+})
+frontPlacement.setParent(stand)
 
-agent.addPlacement(placement1).start().then()
+const backPlacement = new PlainPlacement('Stand back', {
+    position: new Vector3(0, 2.4, 0.28),
+    rotation: Quaternion.Euler(0, 180, 0),
+    width: 1.88,
+    ratio: '9:16',
+    types: ['image', 'video'],
+    mimes: ['image/jpeg', 'image/png', 'video/mp4'],
+})
+backPlacement.setParent(stand)
 
-//
-// engine.addEntity(unit1);
-// adsharesBanner.spawn(
-//    unit1,
-//    {
-//      payout_network: 'ads',
-//      payout_address: '0001-000000F1-6451',
-//      keywords: 'decentraland,metaverse',
-//      zone_name: 'default',
-//      adserver: 'https://app.web3ads.net',
-//      exclude: '{"quality": ["low"], "category": ["adult"]}',
-//    }
-// )
-
-// const adsharesBanner = new AdsharesBanner();
-// const unit2 = new Entity('unit2');
-// unit2.addComponent(new Transform({
-//    position: new Vector3(6, 0, 6),
-//    scale: new Vector3(2, 2, 0.1),
-// }));
-// engine.addEntity(unit2);
-// adsharesBanner.spawn(
-//    unit2,
-//    {
-//        payout_network: 'bsc',
-//        payout_address: '0xcfcecfe2bd2fed07a9145222e8a7ad9cf1ccd22a', // put your metamask address here (binance chain)
-//        keywords: 'decentraland,metaverse',
-//        zone_name: 'default',
-//        adserver: 'https://app.web3ads.net',
-//        exclude: '{"quality": ["low"], "category": ["adult"]}',
-//    }
-// )
+engine.addEntity(stand)
+agent.addPlacement(frontPlacement, backPlacement).spawn()
