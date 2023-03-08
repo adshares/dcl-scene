8443f7828825c840996db3f938a226f452e17070
Maciej Pilarczyk <m.pilarczyk@outlook.com>
2023-03-08 12:50:38 +0100

    Refactor

--- src/game.ts
+++ src/game.ts
@@ -1,30 +1,28 @@
-import {PlainPlacement, SupplyAgent} from '../node_modules/@adshares/decentraland/src/index'
+import * as Ads from '../node_modules/@adshares/decentraland/src/index'
 
-const agent = SupplyAgent.fromWallet('https://app.web3ads.net', 'ads', '0001-00000000-9B6F')
-// const agent = new SupplyAgent('https://app.web3ads.net', 'e39f6593-578e-41f0-8d06-88aff41c6a19')
+const agent = Ads.SupplyAgent.fromWallet('https://app.web3ads.net', 'ads', '0001-00000000-9B6F')
+// const agent = new Ads.SupplyAgent('https://app.web3ads.net', 'e39f6593-578e-41f0-8d06-88aff41c6a19')
 
 const stand = new Entity()
 stand.addComponent(new GLTFShape('models/stand.glb'))
 stand.addComponent(new Transform({
-    position: new Vector3(8, 0, 8),
+  position: new Vector3(8, 0, 8),
 }))
 
-const frontPlacement = new PlainPlacement('Stand front', {
-    position: new Vector3(0, 2.4, -0.28),
-    width: 1.88,
-    ratio: '9:16',
-    background: null,
+const frontPlacement = new Ads.PlainPlacement('Stand front', {
+  position: new Vector3(0, 2.355, -0.288546),
+  width: 1.65,
+  ratio: '9:16',
 })
 frontPlacement.setParent(stand)
 
-const backPlacement = new PlainPlacement('Stand back', {
-    position: new Vector3(0, 2.4, 0.28),
-    rotation: Quaternion.Euler(0, 180, 0),
-    width: 1.88,
-    ratio: '9:16',
-    types: ['image', 'video'],
-    mimes: ['image/jpeg', 'image/png', 'video/mp4'],
-    background: null,
+const backPlacement = new Ads.PlainPlacement('Stand back', {
+  position: new Vector3(0, 2.355, 0.288546),
+  rotation: Quaternion.Euler(0, 180, 0),
+  width: 1.65,
+  ratio: '9:16',
+  types: ['image'],
+  mimes: ['image/jpeg', 'image/png'],
 })
 backPlacement.setParent(stand)
 
